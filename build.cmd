1b1f5f9753ec644589198f31a81b5a80e12b65fb
Babken Vardanyan <483ken@gmail.com>
2014-08-31 15:52:34 +0400

    fixed dist dir name

--- build.cmd
+++ build.cmd
@@ -1,8 +1,8 @@
 @echo off
 python C:\Python34\Scripts\cxfreeze --base-name=Win32GUI ros_config_monitoring.py --target-dir "RouterOS Configuration Monitoring"
-copy router_icon.png dist
-copy users.xml dist
-copy report.tex dist
+copy router_icon.png "RouterOS Configuration Monitoring"
+copy users.xml "RouterOS Configuration Monitoring"
+copy report.tex "RouterOS Configuration Monitoring"
 echo "The executable and all necessary files was put in dist folder."
 echo "You only have to install Miktex or extract the Miktex portable installer into miktex_portable directory and put that directory in dist"
 pause
