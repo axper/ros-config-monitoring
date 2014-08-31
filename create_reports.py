9cc73d60e9b2f01b95ef451f43e99e82fb0e59a8
Babken Vardanyan <483ken@gmail.com>
2014-08-31 09:30:37 +0400

    create_reports.py: ported to python3; modulized

--- create_reports.py
+++ create_reports.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8
 
 '''
@@ -23,7 +23,6 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-from __future__ import print_function
 import sys
 import time
 import subprocess
@@ -33,112 +32,108 @@ import xml.etree.ElementTree
 
 
 
-def create_backup(filename):
-    ''' Copies filename into backup directory and returns full path of it
-    '''
-    backup_dir = 'backups'
+class ReportCreator(object):
+    def create_backup(self, filename):
+        ''' Copies filename into backup directory and returns full path of it
+        '''
+        backup_dir = 'backups'
 
-    if not os.path.exists(backup_dir):
-        os.makedirs(backup_dir)
+        if not os.path.exists(backup_dir):
+            os.makedirs(backup_dir)
 
-    backup_filename = time.strftime('%d.%m.%Y_%H.%M.%S') + '__' + filename
-    backup_full_path = os.path.join(backup_dir, backup_filename)
-    shutil.copy2(filename, backup_full_path)
+        backup_filename = time.strftime('%d.%m.%Y_%H.%M.%S') + '__' + filename
+        backup_full_path = os.path.join(backup_dir, backup_filename)
+        shutil.copy2(filename, backup_full_path)
 
-    return backup_full_path
+        return backup_full_path
 
-
-def move(source, destination):
-    ''' Moves/Renames file source to destination '''
-    try:
-        os.remove(destination)
-    except OSError:
-        pass
-
-    shutil.copyfile(source, destination)
-
-    os.remove(source)
-
-
-def get_users_from_file(filename):
-    ''' Reads and returns user info dictionary from file filename '''
-    users_dict = {}
-
-    tree = xml.etree.ElementTree.parse(filename)
-    root = tree.getroot()
-
-    for user_info in root:
-        username = user_info.get('username')
-        realname = user_info.find('realname').text.encode('utf-8')
-
-        users_dict[username] = realname
-
-    return users_dict
-
-
-def compile_latex(filename_tex):
-    ''' Compiles the filename_tex to PDF using xelatex
-        LaTeX distribution is first searched in $PATH.
-        If not found, it looks for a local miktex_portable directory.
-    '''
-    try:
-        subprocess.call(['xelatex', filename_tex])
-    except OSError:
-        print('Did not find xelatex in $PATH, looking for dir miktex_porable')
+    def move(self, source, destination):
+        ''' Moves/Renames file source to destination '''
         try:
-            subprocess.call(['miktex_portable\\miktex\\bin\\xelatex.exe',
-                             filename_tex])
+            os.remove(destination)
         except OSError:
-            print('Could not find local porable directory either. Aborting')
-            sys.exit(1)
+            pass
+
+        shutil.copyfile(source, destination)
+
+        os.remove(source)
+
+    def get_users_from_file(self, filename):
+        ''' Reads and returns user info dictionary from file filename '''
+        users_dict = {}
+
+        tree = xml.etree.ElementTree.parse(filename)
+        root = tree.getroot()
+
+        for user_info in root:
+            username = user_info.get('username')
+            realname = user_info.find('realname').text.encode('utf-8')
+
+            users_dict[username] = realname
+
+        return users_dict
+
+    def compile_latex(self, filename_tex):
+        ''' Compiles the filename_tex to PDF using xelatex
+            LaTeX distribution is first searched in $PATH.
+            If not found, it looks for a local miktex_portable directory.
+        '''
+        try:
+            subprocess.call(['xelatex', filename_tex])
+        except OSError:
+            print('Did not find xelatex in $PATH, looking for dir miktex_porable')
+            try:
+                subprocess.call(['miktex_portable\\miktex\\bin\\xelatex.exe',
+                                 filename_tex])
+            except OSError:
+                print('Could not find local porable directory either. Aborting')
+                sys.exit(1)
+
+    def view_file(self, filename):
+        ''' Opens a file in OS's default file viewer '''
+        if sys.platform.startswith('linux'):
+            subprocess.call(['xdg-open', filename])
+        else:
+            os.startfile(filename)
+
+    def create_report_for_user(self, user, realname):
+        ''' Reads user's diff file and generates PDF report and opens the file '''
+        print('Generating report for "' + user +
+              '" (' + realname + '):')
+
+        filename_user_diff = user + '_diff.txt'
+
+        if not os.path.exists(filename_user_diff):
+            print('No diff file found for ' + user + ', skipping...')
+            return
 
 
-def view_file(filename):
-    ''' Opens a file in OS's default file viewer '''
-    if sys.platform.startswith('linux'):
-        subprocess.call(['xdg-open', filename])
-    else:
-        os.startfile(filename)
+        self.create_backup(filename_user_diff)
+        self.move(filename_user_diff, 'diff.txt')
 
 
-def create_report(user, realname):
-    ''' Reads user's diff file and generates PDF report and opens the file '''
-    print('Generating report for "' + user +
-          '" (' + realname.encode('utf-8') + '):')
+        # Write users's real name into 'nameit.txt' (for LaTeX)
+        with open('nameit.txt', 'w') as nameit:
+            nameit.write(realname)
 
-    filename_user_diff = user + '_diff.txt'
+        self.compile_latex('report.tex')
 
-    if not os.path.exists(filename_user_diff):
-        print('No diff file found for ' + user + ', skipping...')
-        return
-
-
-    create_backup(filename_user_diff)
-    move(filename_user_diff, 'diff.txt')
+        filename_user_report = user + '_report.pdf'
+        move('report.pdf', filename_user_report)
+        file_report_backup = create_backup(filename_user_report)
 
 
-    # Write users's real name into 'nameit.txt' (for LaTeX)
-    with open('nameit.txt', 'w') as nameit:
-        nameit.write(realname.encode('utf-8'))
+        view_file(file_report_backup)
 
-    compile_latex('report.tex')
+    def create_reports(self):
+        ''' Call this '''
+        users = self.get_users_from_file('users.xml')
 
-    filename_user_report = user + '_report.pdf'
-    move('report.pdf', filename_user_report)
-    file_report_backup = create_backup(filename_user_report)
-
-
-    view_file(file_report_backup)
-
-
-def main():
-    ''' Main function '''
-    users = get_users_from_file('users.xml')
-
-    for user in users.keys():
-        create_report(user, users[user].decode('utf-8'))
+        for user in users.keys():
+            self.create_report_for_user(user, users[user].decode('utf-8'))
 
 
 if __name__ == '__main__':
-    main()
+    rc = ReportCreator()
+    rc.create_reports()
 
